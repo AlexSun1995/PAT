96c138ac7d9868acae6abc2a1504302f644b5166
AlexSun1995 <suncun1995@gmail.com>
2017-02-21 18:41:48 +0800

    time out

---
+++ 1044_shopping_in_mars.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include<vector>
+using namespace std;
+#define MAX 666666
+vector<int> vec;
+int n;
+int target;
+int main()
+{
+  freopen("/home/alexsun/in.txt","r",stdin);
+  scanf("%d%d",&n,&target);
+  vec.resize(n);
+  vector<pair<int,int> > ans;
+  int tmp;
+  int meet = MAX;
+  for(int i=0;i<n;i++){
+     scanf("%d",&vec[i]);
+  }
+  for(int i=0;i<n;i++){
+	  pair<int,int> tp;
+	  tmp = 0;
+	  tp.first = i+1;
+	  for(int j=i;j<n;j++){
+		  tmp+=vec[j];
+		  if(tmp>=target&&tmp<meet){
+			  ans.clear();
+			  meet = tmp;
+              tp.second = j+1;
+			  ans.push_back(tp);
+			  break;
+		  }
+		  else if(tmp>=target&&tmp==meet){
+			  tp.second = j+1;
+              ans.push_back(tp);
+              break;
+		  }
+	  } 
+  }
+  for(int i=0;i<ans.size();i++){
+	  printf("%d-%d\n",ans[i].first,ans[i].second);
+  }
+  return 0;
+}
+
