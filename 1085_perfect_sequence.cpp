59151c8aa37bd0d9329f70f740d80b8b008e1e58
AlexSun1995 <suncun1995@gmail.com>
2017-02-21 12:02:15 +0800

    second version,using b-search by self, 24/25,still not perfect

--- 1085_perfect_sequence.cpp
+++ 1085_perfect_sequence.cpp
@@ -1,61 +1,49 @@
-//23/25 I've got no idea what's the matter..
-//this version in not accepted, it's not perfect,
-//if the sequece is like #####*********** which '#' is
-//the result element,then the code will not get the 
-//correct answer,the correct way to do this problem is 
-//search every maxinum value of each element,ans print
-//the maxinum answer,use b-search is a good solution.
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <string>
-#include<queue>
+// pat 1085,perfect sequence
+// this version got 24/5 using b-search...
+// still got no idea.. 
+#include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
-int main()
-{
- freopen("/home/alexsun/in.txt","r",stdin);
- vector<long long> vec;
- long long  n,p;
- long long  ans;
- scanf("%lld %lld",&n,&p);
- vec.resize(n);
- for(int i=0;i<n;i++){
-	 scanf("%lld",&vec[i]);
- }
- sort(vec.begin(),vec.end());
- ans = n;
- int m,M;
- m = 0;
- M = n-1;
- while(m<=M){
-   if(vec[m]*p>=vec[M]){
-	   break;
-   }
-   else{
-	   if(m+1<=M){
-          if(vec[M]<=vec[m+1]*p)
-		  {
-			  ans--;
-			  break;
-		  }
-	   }
-	   if(M-1>=m){
-           if(vec[M-1]<=vec[m]*p){
-			   ans--;
-			   break;
-		   }
-	   }
-      if(m==M){
-        ans--;
-        break;
-      }
-      ans-=2;
-      m++;
-      M--;
-   }
- }
+vector<long long>vec;
+int n;
+int ans=-111;
+long long p;
+// search the element satisify M<vec[pos]*p,where M is as 
+// large as possible from index start to n-1 in vec
+// and return (index of M) - pos + 1
+int bsearch(int pos,int start){
+    if(start>=n) return -111;
+	int left = start;
+	int m = vec[pos];
+	int right = n-1;
+	int mid  = left + (right - left)/2;
+	while(true){
+		mid  = left +(right - left)/2;
+		if(vec[mid]<=(long long)m*p&&mid+1<=right&&vec[mid+1]>(long long)m*p){
+           break; 
+		}
+		else if(vec[mid]<=(long long)m*p&&mid+1>right){
+			break;
+		}
+		else if(vec[mid]<=(long long)m*p){
+            left = mid + 1;
+		}
+		else if(vec[mid]>(long long)m*p){
+			right = mid - 1;
+		}
+	}
+	return mid - pos + 1;
+}
+int main(){
+  freopen("/home/alexsun/in.txt","r",stdin);
+  scanf("%d%lld",&n,&p);
+  vec.resize(n);
+  for(int i=0;i<n;i++) scanf("%lld",&vec[i]);
+  sort(vec.begin(),vec.end());
+  for(int i=0;i<n;i++){
+    ans =  max(ans,bsearch(i,i+1));
+  }
   cout<<ans<<endl;
   return 0;
 }
-
